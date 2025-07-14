2619fbe0cd56d3586799937b102290b4a5736b43
marcusdev <danielmarcusq.felixmena@gmail.com>
2025-07-14 10:52:13 +0800

    Added additional REST APIs

--- src/main/java/com/marcus/Employee/Controller/DepartmentController.java
+++ src/main/java/com/marcus/Employee/Controller/DepartmentController.java
@@ -1,12 +1,15 @@
 package com.marcus.Employee.Controller;
 
+import com.marcus.Employee.Dto.DepartmentInsertDto;
 import com.marcus.Employee.Entity.Department;
+import com.marcus.Employee.Exception.ResourceNotFoundException;
 import com.marcus.Employee.Repository.DepartmentRepository;
+import com.marcus.Employee.Response.ApiResponse;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +23,43 @@ public class DepartmentController {
     @GetMapping
     public List<Department> getDepartment(){
         return departmentRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Department> getDepartmentById(@PathVariable Integer id){
+        Department department = departmentRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No ID found", HttpStatus.NOT_FOUND.value()));
+
+        return ResponseEntity.ok(department);
+    }
+
+    @PostMapping
+    public ResponseEntity<ApiResponse> createDepartment(@Valid @RequestBody DepartmentInsertDto department){
+        Department newDep = department.mapToDepartment(department);
+        departmentRepository.save(newDep);
+        return ResponseEntity.ok(new ApiResponse("Successfully Added", HttpStatus.OK.value()));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponse> updateDepartment(
+            @PathVariable Integer id,
+            @Valid @RequestBody DepartmentInsertDto department){
+
+        Department existingDept = departmentRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No ID found", HttpStatus.NOT_FOUND.value()));
+
+        existingDept.setName(department.getName());
+        departmentRepository.save(existingDept);
+
+        return ResponseEntity.ok(new ApiResponse("Successfully Updated", HttpStatus.OK.value()));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse> deleteDepartment(@PathVariable Integer id){
+        Department existingDept = departmentRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No ID found", HttpStatus.NOT_FOUND.value()));
+
+        departmentRepository.delete(existingDept);
+        return ResponseEntity.ok(new ApiResponse("Successfully Deleted", HttpStatus.OK.value()));
     }
 }
