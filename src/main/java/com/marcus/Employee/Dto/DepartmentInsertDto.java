848ffe07d8a0b922d1ad54eb2f02436f9b5119ee
marcusdev <danielmarcusq.felixmena@gmail.com>
2025-07-14 10:53:14 +0800

    Added validation for empty. Add a method which converts Dto to Department object

--- src/main/java/com/marcus/Employee/Dto/DepartmentInsertDto.java
+++ src/main/java/com/marcus/Employee/Dto/DepartmentInsertDto.java
@@ -1,10 +1,20 @@
 package com.marcus.Employee.Dto;
 
+import com.marcus.Employee.Entity.Department;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
 public class DepartmentInsertDto {
+    @NotEmpty(message = "Name field is required")
     private String name;
+
+    // Mapping Dto to Department class
+    public Department mapToDepartment(DepartmentInsertDto department){
+        Department newDep = new Department();
+        newDep.setName(department.getName());
+        return newDep;
+    }
 }
